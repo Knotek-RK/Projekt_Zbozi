906aff841ac0e1637896269f49082d894145cea2
Robert Knotek <knotyrk@seznam.cz>
2023-01-26 11:26:52 +0100

    Upgrading class ProductService.
    Creating methods.

--- src/main/java/zbozi/eshop/ProductService.java
+++ src/main/java/zbozi/eshop/ProductService.java
@@ -1,8 +1,8 @@
 package zbozi.eshop;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ProductService {
 
@@ -16,5 +16,69 @@ public class ProductService {
         );
     }
 
+    public List<Product> loadAllAvailableItems() throws SQLException {
+        Statement statement = connection.createStatement();
 
+        ResultSet resultSet = statement.executeQuery(
+                "SELECT * FROM product");
+
+        List<Product> resultList = new ArrayList<>();
+
+        while (resultSet.next()) {
+            Product product = extractProductData(resultSet);
+
+            resultList.add(product);
+        }
+
+        return resultList;
+    }
+
+    private static Product extractProductData(ResultSet resultSet) throws SQLException {
+        return new Product(
+                resultSet.getLong("id"),
+                resultSet.getInt("partNo"),
+                resultSet.getString("name"),
+                resultSet.getString("description"),
+                resultSet.getBoolean("isForSale"),
+                resultSet.getBigDecimal("price"));
+    }
+
+    public Product loadProductById(Long itemId) throws SQLException {
+        Statement statement = connection.createStatement();
+
+        ResultSet resultSet = statement.executeQuery(
+                "SELECT * FROM product WHERE id = " + itemId);
+
+        if (resultSet.next()) {
+            return extractProductData(resultSet);
+        }
+
+        return null;
+    }
+
+    public Long saveNewItem(Product product) throws SQLException {
+        Statement statement = connection.createStatement();
+
+        statement.executeUpdate(
+                "INSERT INTO product(partNo, name, description, isForSale, price) VALUES ('"+
+                        newProduct.getPartNo() + "', " + newProduct.getName() + "', " + newProduct.getDescription() +
+                        "', " + newProduct.getForSale() + "', " + newProduct.getPrice() +")",
+                Statement.RETURN_GENERATED_KEYS);
+
+        return statement.getGeneratedKeys().getLong("id");
+    }
+
+    public void updatePriceById(Long itemId) throws SQLException {
+        Statement statement = connection.createStatement();
+
+        statement.executeUpdate(
+                "UPDATE product SET price WHERE id = " + itemId);
+    }
+
+    public void deleteOutOfSaleItems(Boolean IsForSale) throws  SQLException {
+        Statement statement = connection.createStatement();
+
+        statement.executeUpdate(
+                "DELETE FROM * product WHERE isForSale = FALSE");
+    }
 }
